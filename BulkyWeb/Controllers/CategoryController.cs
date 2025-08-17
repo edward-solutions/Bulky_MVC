24b58194b2639a5f37ab3f856601229750eecf2e
edward cabanatan <edward.cabanatan@gmail.com>
2025-08-17 22:54:53 +0800

    Add validation for category name and display order
    Updated the `Create` action in `CategoryController.cs` to prevent the category `Name` from matching its `DisplayOrder`. Added a model error for this validation case.
    
    Enhanced `Create.cshtml` by including a validation summary div to display all validation errors, improving user experience during category creation.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,6 +25,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display order cannot exactly match the name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
