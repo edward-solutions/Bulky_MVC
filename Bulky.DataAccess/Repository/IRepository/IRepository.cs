c3550b8b6d47e617c5cacc06216e7217ccc7b427
edward cabanatan <edward.cabanatan@gmail.com>
2025-08-21 23:35:14 +0800

    Add IRepository<T> interface for CRUD operations
    Introduces a generic `IRepository<T>` interface in the
    `Bulky.DataAccess.Repository.IRepository` namespace.
    This interface includes methods for basic CRUD operations:
    `GetAll()`, `Get(Expression<Func<T, bool>> filter)`,
    `Add(T entity)`, `Delete(T entity)`, and
    `DeleteRange(IEnumerable<T> entityList)`.
    Additional using directives are added for .NET functionalities
    such as collections, LINQ, and expressions.

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        //if "T" is Category table
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter); //this is how we filter by LINQ to an entity
+        void Add(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entityList); //for deleting multiple records at once
+    }
+}
