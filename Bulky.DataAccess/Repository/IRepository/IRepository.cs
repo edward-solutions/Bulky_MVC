70942489b73ac70ad144a7e6337c16af00bc81c4
edward cabanatan <edward.cabanatan@gmail.com>
2025-08-22 14:44:10 +0800

    Implement IRepository interface

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         //if "T" is Category table
         IEnumerable<T> GetAll();
         T Get(Expression<Func<T, bool>> filter); //this is how we filter by LINQ to an entity
         void Add(T entity);
-        void Delete(T entity);
-        void DeleteRange(IEnumerable<T> entityList); //for deleting multiple records at once
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entityList); //for deleting multiple records at once
     }
 }
